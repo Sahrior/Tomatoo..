f65db257f852dc57b8870b2c9c31de3eaeae559b
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-03-21 21:53:35 +0600

    header added

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header' >
+      
+        <dib className="header-content">
+            
+            <h2>Order Your Favourite food here</h2>
+            <p>At our restaurant, we offer a diverse menu featuring rich flavors from multiple cuisines, ensuring something for everyone. Whether you crave spicy, sweet, or healthy options, our carefully crafted dishes guarantee a satisfying and memorable dining experience.</p>
+
+            <button>View Menu</button>
+        </dib>
+
+    </div>
+  )
+}
+
+export default Header
